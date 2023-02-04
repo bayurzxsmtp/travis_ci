c97a38252c456d798e4fab706f91473bb7a704b3
bayurzxsmtp <bayurzxsmtp@gmail.com>
2023-02-04 22:34:45 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log('Containers rule!');
+    await sleep(8000);
+  }
+}
+
+main();
